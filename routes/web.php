2c0f251a0b254d648a4e18b44caa8b35a27c5b50
Ananda Reza <Rezzz1120@gmail.com>
2025-09-10 11:39:28 +0700

    mendesain layouy

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('');
+    return view('halamanumum');
 });
 
 Route::get('/dashboard', function () {
